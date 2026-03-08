c706526e70aaa3bd812b5cdd4a5f6169cb5d9601
VM-fll <80290089+VM-fll@users.noreply.github.com>
2026-03-08 14:58:35 -0400

    basic vision subsystem + vendor lib

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -18,15 +18,12 @@ public class VisionConstants {
       AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
 
   // Camera names, must match names configured on coprocessor
-  public static String camera0Name = "camera_0";
-  public static String camera1Name = "camera_1";
+  public static String camera0Name = "alarmcamera0";
 
   // Robot to camera transforms
   // (Not used by Limelight, configure in web UI instead)
   public static Transform3d robotToCamera0 =
-      new Transform3d(0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, 0.0));
-  public static Transform3d robotToCamera1 =
-      new Transform3d(-0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI));
+      new Transform3d(1.5 / 39.37, 13 / 39.37, 7.75 / 39.37, new Rotation3d(0.0, Math.PI / 4, 0));
 
   // Basic filtering thresholds
   public static double maxAmbiguity = 0.3;
